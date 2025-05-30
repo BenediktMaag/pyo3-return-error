661e91d57db6aedee6db12ae4a3bdfab06f4019a
Benedikt Maag <Benedikt.Maag@LandisGyr.com>
2025-05-30 11:32:28 +0200

    repository that wont compile on pyo3 0.25.0

---
+++ src/lib.rs
@@ -0,0 +1,14 @@
+use chrono::{DateTime, Local, TimeZone};
+use pyo3::prelude::*;
+
+#[pyfunction]
+fn get_timestamp() -> PyResult<Option<DateTime<Local>>> {
+    Ok(Local.with_ymd_and_hms(2025, 05, 30, 12, 30, 34).single())
+}
+
+/// A Python module implemented in Rust.
+#[pymodule]
+fn pyo3_return_error(m: &Bound<'_, PyModule>) -> PyResult<()> {
+    m.add_function(wrap_pyfunction!(get_timestamp, m)?)?;
+    Ok(())
+}
