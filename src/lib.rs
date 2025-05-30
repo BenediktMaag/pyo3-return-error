0731ae666057042bdde77420a2166344ff12c559
Benedikt Maag <Benedikt.Maag@LandisGyr.com>
2025-05-30 11:46:27 +0200

    simplify the example by removing the result as it still shows the same error

--- src/lib.rs
+++ src/lib.rs
@@ -2,8 +2,8 @@ use chrono::{DateTime, Local, TimeZone};
 use pyo3::prelude::*;
 
 #[pyfunction]
-fn get_timestamp() -> PyResult<Option<DateTime<Local>>> {
-    Ok(Local.with_ymd_and_hms(2025, 05, 30, 12, 30, 34).single())
+fn get_timestamp() -> Option<DateTime<Local>> {
+    Local.with_ymd_and_hms(2025, 05, 30, 12, 30, 34).single()
 }
 
 /// A Python module implemented in Rust.
